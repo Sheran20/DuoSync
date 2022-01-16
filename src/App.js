f913267fab2479753cf37663c92e60294eac2ec3
Sheran <gt-sherantv@hotmail.com>
2022-01-16 00:13:37 -0500

    update app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React, { Component } from "react";
+import Controller from './Components/Controller';
 
-class App extends Component{
+class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello World</h1>
+        <Controller/>
       </div>
     )
   }
